b6db39461c8f3b26bd270406e386c372b0a6f4f2
Ashiegbu <chibuch@gmail.com>
2023-01-22 08:52:22 -0800

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,11 +7,11 @@
  */
 void swap_ints(int *a, int *b)
 {
-  int tmp;
+int tmp;
 
-  tmp = *a;
-  *a = *b;
-  *b = tmp;
+tmp = *a;
+*a = *b;
+*b = tmp;
 }
 
 /**
@@ -23,24 +23,24 @@ void swap_ints(int *a, int *b)
  */
 void bubble_sort(int *array, size_t size)
 {
-  size_t i, len = size;
-  bool bubbly = false;
+size_t i, len = size;
+bool bubbly = false;
 
-  if (array == NULL || size < 2)
-    return;
+if (array == NULL || size < 2)
+return;
 
-  while (bubbly == false)
-    {
-      bubbly = true;
-      for (i = 0; i < len - 1; i++)
-	{
-	  if (array[i] > array[i + 1])
-	    {
-	      swap_ints(array + i, array + i + 1);
-	      print_array(array, size);
-	      bubbly = false;
-	    }
-	}
-      len--;
-    }
+while (bubbly == false)
+{
+bubbly = true;
+for (i = 0; i < len - 1; i++)
+{
+if (array[i] > array[i + 1])
+{
+swap_ints(array + i, array + i + 1);
+print_array(array, size);
+bubbly = false;
+}
+}
+len--;
+}
 }
